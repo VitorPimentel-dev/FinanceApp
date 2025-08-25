f1727de263045bc4fe44857918c6988d23348d3c
Vitor Pimentel <vitorpimentel493@gmail.com>
2025-08-24 21:29:49 -0300

    implementação do service CategorieService

--- FinanceApp.Domain/Services/CategorieService.cs
+++ FinanceApp.Domain/Services/CategorieService.cs
@@ -1,12 +1,37 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using FinanceApp.Domain.Entities;
+using FinanceApp.Domain.Interfaces;
 
 namespace FinanceApp.Domain.Services
 {
-    public class CategorieService
+    public class CategorieService : IRepository<Categorie>
     {
-        
+        User User = new User();
+        public CategorieService(User user)
+        {
+            User = user;
+        }
+        public void Add(Categorie entity)
+        {
+            User.AddCategory(entity);
+        }
+
+        public IEnumerable<Categorie> GetAll()
+        {
+            return User.Categories;
+        }
+
+        public void Update(Categorie entity)
+        {
+            var index = User.Categories.FindIndex(c => c.Id == entity.Id);
+            if (index != -1)
+            {
+                User.Categories[index] = entity;
+            }
+        }
+
+        public void Delete(Categorie entity)
+        {
+            User.Categories.RemoveAll(c => c.Id == entity.Id);
+        }
     }
 }
