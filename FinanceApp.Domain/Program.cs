74a7e7491e3f083a10bcae8175879e93c4010066
Vitor Pimentel <vitorpimentel493@gmail.com>
2025-08-24 21:28:33 -0300

    Add CategorieService

--- FinanceApp.Domain/Program.cs
+++ FinanceApp.Domain/Program.cs
@@ -1,38 +1,73 @@
-﻿using FinanceApp.Domain.Entities;
+﻿using System;
+using System.Linq;
+using FinanceApp.Domain.Entities;
 using FinanceApp.Domain.Services;
 
-UserService userService = new UserService();
-System.Console.WriteLine("Login/cadastro");
-if (Console.ReadLine()!.ToLower() == "login")
+class Program
 {
-    System.Console.WriteLine("Digite o email do usuario:");
-    string email = Console.ReadLine()!;
-    System.Console.WriteLine("Digite a senha do usuario:");
-    string password = Console.ReadLine()!;
-    var user = userService.GetByEmail(email);
-    if (user == null || !user.ValidatePassword(password))
+    static void Main()
     {
-        System.Console.WriteLine("Usuario ou senha inválidos.");
-        return;
-    }
-    System.Console.WriteLine($"Bem vindo {user.Email}!");
+        var userService = new UserService();
 
-    TransactionService transactionService = new TransactionService(user);
-    System.Console.WriteLine("Digite o valor da transação:");
-    double amount = double.Parse(Console.ReadLine()!);
-    DateTime date = DateTime.Now;
-    var transaction = new Transaction(amount, date, user.Categories.First());
-    transactionService.Add(transaction);
-    System.Console.WriteLine("Transação adicionada com sucesso!");
-    System.Console.WriteLine(user);
-}
-else
-{
-    System.Console.WriteLine("Digite o email do usuario:");
-    string email = Console.ReadLine()!;
-    System.Console.WriteLine("Digite a senha do usuario:");
-    string password = Console.ReadLine()!;
-    userService.Add(new User(email, password));
-    var user = userService.GetByEmail(email);
-    System.Console.WriteLine($"Usuario {user.Email} cadastrado com sucesso!");
+        Console.WriteLine("Digite 'login' para entrar ou 'cadastro' para criar um usuário:");
+        var opcao = Console.ReadLine()?.ToLower();
+
+        if (opcao == "cadastro")
+        {
+            Console.WriteLine("Digite o email do usuário:");
+            string email = Console.ReadLine()!;
+            Console.WriteLine("Digite a senha do usuário:");
+            string password = Console.ReadLine()!;
+
+            if (userService.GetByEmail(email) != null)
+            {
+                Console.WriteLine("Email já cadastrado.");
+                return;
+            }
+
+            var user = new User(email, password);
+            userService.Add(user);
+            Console.WriteLine($"Usuário {user.Email} cadastrado com sucesso!");
+        }
+
+        // Login
+        Console.WriteLine("Digite o email do usuário:");
+        string loginEmail = Console.ReadLine()!;
+        Console.WriteLine("Digite a senha do usuário:");
+        string loginPassword = Console.ReadLine()!;
+
+        var loggedUser = userService.GetByEmail(loginEmail);
+        if (loggedUser == null || !loggedUser.ValidatePassword(loginPassword))
+        {
+            Console.WriteLine("Usuário ou senha inválidos.");
+            return;
+        }
+
+        Console.WriteLine($"Bem-vindo {loggedUser.Email}!");
+
+        if (!loggedUser.Categories.Any())
+        {
+            var categoriaPadrao = new Categorie("Lazer", "Atividades de lazer e entretenimento");
+            loggedUser.AddCategory(categoriaPadrao);
+        }
+
+        var transactionService = new TransactionService(loggedUser);
+
+        Console.WriteLine("Digite o valor da transação:");
+        if (!double.TryParse(Console.ReadLine(), out double amount))
+        {
+            Console.WriteLine("Valor inválido.");
+            return;
+        }
+        System.Console.WriteLine("Digite o nome da categoria");
+        var categoria = new Categorie(Console.ReadLine()!, "Categoria personalizada");
+        loggedUser.AddCategory(categoria);
+        var transaction = new Transaction(amount, DateTime.Now, categoria);
+        transactionService.Add(transaction);
+
+        Console.WriteLine("Transação adicionada com sucesso!\n");
+
+        // Exibe usuário com transações
+        Console.WriteLine(loggedUser);
+    }
 }
---
+++ FinanceApp.Domain/Services/CategorieService.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinanceApp.Domain.Services
+{
+    public class CategorieService
+    {
+        
+    }
+}
